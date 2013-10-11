55a66edfdf989a98d076100c8ea6cceb91f10cf4
Damian Karzon <dkarzon@tboda.com>
2013-10-11 15:41:27 +1300

    Cat gifs...

--- MMBot/Scripts/Cats.cs
+++ MMBot/Scripts/Cats.cs
@@ -30,12 +30,45 @@ namespace MMBot.Scripts
 
                 await CatMeCore(msg, number);
             });
+
+            robot.Respond(@"(cat|cats)( gif) (.*)?", async msg =>
+            {
+                int number = 1;
+                try
+                {
+                    number = Int32.Parse(msg.Match[3]);
+                }
+                catch (Exception) { }
+                if (number == 0)
+                {
+                    number = 1;
+                }
+
+                await CatMeGifCore(msg, number);
+            });
         }
 
         private static async Task CatMeCore(IResponse<TextMessage> msg, int number)
         {
-            var xDoc = await msg.Http(string.Format(Url, number))
-                .GetXml();
+            var xDoc = await msg.Http(string.Format(Url, number) + "&type=gif").GetXml();
+
+            try
+            {
+                var urls = xDoc.SelectNodes("//url");
+                foreach (XmlNode url in urls)
+                {
+                    msg.Send(url.InnerText);
+                }
+            }
+            catch (Exception)
+            {
+                msg.Send("erm....issues, move along");
+            }
+        }
+
+        private static async Task CatMeGifCore(IResponse<TextMessage> msg, int number)
+        {
+            var xDoc = await msg.Http(string.Format(Url, number)).GetXml();
 
             try
             {
@@ -56,7 +89,9 @@ namespace MMBot.Scripts
             return new[]
             {
                 "mmbot cat me <number> - Returns a number of cat pictures.",
-                "mmbot cat me - Returns a cat picture."
+                "mmbot cat me - Returns a cat picture.",
+                "mmbot cat gif <number> - Returns a number of cat gifs.",
+                "mmbot cat gif - Returns a cat gif."
             };
         }
     }
