ad53be385ec0176de67f9ecf4fce1622b68d0bee
PeteGoo <pete@petegoo.com>
2013-10-12 17:02:34 +1300

    Reset to Jabbr as default

--- MMBot.Runner/Program.cs
+++ MMBot.Runner/Program.cs
@@ -49,8 +49,8 @@ namespace MMBot.Runner
             // If not configured via dictionary then matching environment vars will be used
 
             // Uncomment the appropriate line below to use Jabbr or HipChat
-            //var robot = Robot.Create<JabbrAdapter>("mmbot", config, LoggerConfigurator.GetConsoleLogger(LogLevel.Info));
-            var robot = Robot.Create<HipChatAdapter>("mmbot", config, LoggerConfigurator.GetConsoleLogger(LogLevel.Info));
+            var robot = Robot.Create<JabbrAdapter>("mmbot", config, LoggerConfigurator.GetConsoleLogger(LogLevel.Info));
+            //var robot = Robot.Create<HipChatAdapter>("mmbot", config, LoggerConfigurator.GetConsoleLogger(LogLevel.Info));
             
             //TODO: Discover scripts
             
