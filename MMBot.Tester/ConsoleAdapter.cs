9b2b579a755d02fcd156b13a10a27d4fb685efdb
Damian Karzon <dkarzon@tboda.com>
2013-10-26 15:45:08 -0700

    Added MMBot.Tester as a console app to test scripts a bit easier

---
+++ MMBot.Tester/ConsoleAdapter.cs
@@ -0,0 +1,55 @@
+﻿using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMBot.Tester
+{
+    public class ConsoleAdapter : Adapter
+    {
+        private User _user;
+
+        public ConsoleAdapter(Robot robot, ILog logger)
+            : base(robot, logger)
+        {
+            _user = new User("test");
+        }
+
+        public async override Task Run()
+        {
+            Task.Run(() => StartListening());
+        }
+
+        private void StartListening()
+        {
+            while(true)
+            {
+                var message = Console.ReadLine();
+
+                if (message.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    Environment.Exit(0);
+                    return;
+                }
+                Robot.Receive(new TextMessage(_user, message, null));
+            }
+        }
+
+        public override async Task Send(Envelope envelope, params string[] messages)
+        {
+            await base.Send(envelope, messages);
+
+            foreach (var message in messages.Where(message => !string.IsNullOrWhiteSpace(message)))
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        public override async Task Close()
+        {
+            //Something?
+        }
+    }
+}
