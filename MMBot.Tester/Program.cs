d4cce5dec9f2d43279375d6a478107d71dad4fb6
Damian Karzon <dkarzon@tboda.com>
2013-11-07 23:44:52 +1300

    mmbot scriptthat [gist] #32

--- MMBot.Tester/Program.cs
+++ MMBot.Tester/Program.cs
@@ -18,11 +18,11 @@ namespace MMBot.Tester
 
             robot.LoadScript<ScriptsScripts>();
 
-            robot.AutoLoadScripts = false;
+            //robot.AutoLoadScripts = false;
 
-            robot.LoadScriptFile(@"E:\Code\mmbot\MMBot\Scripts\ping.csx");
+            //robot.LoadScriptFile(@"E:\Code\mmbot\MMBot\Scripts\ping.csx");
 
-            robot.LoadScriptFile(@"E:\Code\mmbot\MMBot\Scripts\whenisay.csx");
+            //robot.LoadScriptFile(@"E:\Code\mmbot\MMBot\Scripts\whenisay.csx");
 
             robot.Run();
 
