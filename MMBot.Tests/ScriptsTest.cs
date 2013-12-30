11c38ceac4ee488e70bbbe0d424a20ccbceb5e82
PeteGoo <pete@petegoo.com>
2013-12-31 12:38:24 +1300

    moved tests to xunit

--- MMBot.Tests/ScriptsTest.cs
+++ MMBot.Tests/ScriptsTest.cs
@@ -1,24 +1,27 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MMBot.Tests.CompiledScripts;
+using Xunit;
 
 namespace MMBot.Tests
 {
-    [TestClass]
     public class ScriptsTest
     {
-        [TestMethod]
+        [Fact]
         public void CanRegisterCompiledScripts()
         {
             var robot = Robot.Create<StubAdapter>();
             robot.LoadScripts(typeof(Ping).Assembly);
         }
 
-        [TestMethod]
+        [Fact]
         public async Task WhenPing_ReceivePong()
         {
             var robot = Robot.Create<StubAdapter>();
+            robot.AutoLoadScripts = false;
             var adapter = robot.Adapters.First().Value as StubAdapter;
             robot.LoadScript<Ping>();
             robot.AutoLoadScripts = false;
@@ -26,14 +29,12 @@ namespace MMBot.Tests
 
             adapter.SimulateMessage("test1", "mmbot ping");
 
-            var messages = adapter.Messages.Select(m => m.Item2);
-            Assert.AreEqual(1, messages.Count());
-            Assert.AreEqual("pong", messages.First().First(), true);
+            var firstMessage = (await adapter.GetEmittedMessages(1)).Select(i => i.Item2).First();
+            Assert.Equal(1, firstMessage.Count());
+            Assert.Equal("pong", firstMessage.First(), StringComparer.InvariantCultureIgnoreCase);
         }
 
-        #region Auth
-
-        [TestMethod]
+        [Fact]
         public async Task Auth_CanAddRemoveUsernameToRole()
         {
             var robot = Robot.Create<StubAdapter>();
@@ -42,20 +43,17 @@ namespace MMBot.Tests
             robot.LoadScriptName("Auth");
             await robot.Run();
 
-            Assert.IsTrue(robot.ScriptData.Any(d => d.Name == "Auth"));
+            Assert.True(robot.ScriptData.Any(d => d.Name == "Auth"));
 
             adapter.SimulateMessage("test1", "mmbot add test1 to the testgroup role");
             adapter.SimulateMessage("test1", "mmbot remove test1 from the testgroup role");
 
-            var messages = adapter.Messages.Select(m => m.Item2);
-            Assert.AreEqual(2, messages.Count());
-            Assert.IsTrue(
-                "Got it, test1 is now in the testgroup role" == messages.First().First() ||
-                "test1 is already in the testgroup role" == messages.First().First());
-            Assert.AreEqual("Got it, test1 is no longer in the testgroup role", messages.Last().First(), true);            
+            var messages = await adapter.GetEmittedMessages(2);
+            Assert.Equal(2, messages.Count());
+            Assert.True(
+                "Got it, test1 is now in the testgroup role" == messages.First().Item2.First() ||
+                "test1 is already in the testgroup role" == messages.First().Item2.First());
+            Assert.Equal("Got it, test1 is no longer in the testgroup role", messages.Last().Item2.First(), StringComparer.InvariantCultureIgnoreCase);
         }
-
-        #endregion
-
     }
 }
