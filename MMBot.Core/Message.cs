65e9795e578fe387dc7b6b8a2c150866934392f4
PeteGoo <pete@petegoo.com>
2014-01-05 21:43:13 +1300

    Fixed deserialization of TextMessage

--- MMBot.Core/Message.cs
+++ MMBot.Core/Message.cs
@@ -14,12 +14,10 @@
 
     public class TextMessage : Message
     {
-        
-
-        public TextMessage(User user, string content, string id)
+        public TextMessage(User user, string text, string id)
         {
             User = user;
-            Text = content;
+            Text = text;
             Id = id;
         }
 
